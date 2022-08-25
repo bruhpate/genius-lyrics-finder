a38728b30092939beb12515d981bd01c123549ae
bruhpate <pietropatelli05@gmail.com>
2022-08-25 14:52:40 +0200

    changes

--- script/main.py
+++ script/main.py
@@ -3,11 +3,10 @@ import re
 
 #IMPORTANT, INSTALL GENIUS MODULE FOR USE IT --> pip install lyricsgenius
 
-#replace the value of this variable with your genius token, or generate one --> https://genius.com/api-clients
-yourToken = "*replace with your token*"
-genius = Genius(yourToken)
+token = "UEYB1W3K8BHWH3LM6eg2pn5H_CYzuV0vmInpzUDSngUJWm_H6s-k3-nFoWKn2EnI"
+genius = Genius(token)
 def ask():
-    print("Search the song: ")
+    print("Search the song: ", end="")
     try:
         song = genius.lyrics(genius.search_song(input()).id, None, False)
         x = re.sub("Embed$", "", song)
