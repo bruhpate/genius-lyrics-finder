8ad572abcc256512f2848b14bb1ade720a7bd883
bruhpate <pietropatelli05@gmail.com>
2022-08-26 11:58:57 +0200

    its boring

--- script/main.py
+++ script/main.py
@@ -6,7 +6,7 @@ import re
 token = "UEYB1W3K8BHWH3LM6eg2pn5H_CYzuV0vmInpzUDSngUJWm_H6s-k3-nFoWKn2EnI"
 genius = Genius(token)
 def ask():
-    print("Search the song: ", end="")
+    print("Search a song: ", end="")
     try:
         song = genius.lyrics(genius.search_song(input()).id, None, False)
         x = re.sub("Embed$", "", song)
